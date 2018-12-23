da06e3f0c0fb3fe3d568a51d4a9d7435ad06b742
houruiqiang <847391877@qq.com>
2018-12-23 16:53:11 +0800

    对接部分详情

--- nationalNuclear/pages/modulars/hse/js/5-10HSE.js
+++ nationalNuclear/pages/modulars/hse/js/5-10HSE.js
@@ -3,14 +3,71 @@ new Vue({
     el: "#app",
     data: {
         showTab: true,
-		recordNo:"",
-		data:[]
+		// recordNo:"",
+		data:[],//流转信息
+		listParam:{},// 列表数据
+		dangerData:{},//隐患信息
+		tabCode:"0",
+		showButton: true,    // 是否显示底部按钮
+		showVerify: false,   //是否显示整改验证
+		showBack: false, // 是否显示撤回按钮
+		showTongBtn: false,  // 是否显示通过与不通过按钮
+		showYanBtn: false,  // 是否显示整改验证按钮
     },
     mounted: function() {
 		_this = this;
 		function plusReady() {
-			_this.recordNo = plus.webview.currentWebview().recordNo;
+			_this.listParam =plus.webview.currentWebview().params;
+			_this.listParam.approveDate = _this.listParam.approveDate.time;
+			_this.listParam.checkDate = _this.listParam.checkDate.time;
+			_this.listParam.draftDate = _this.listParam.draftDate.time;
+// 			console.log(JSON.stringify(_this.listParam))
+// 			console.log(_this.listParam.checkDate.time)
+			_this.tabCode = plus.webview.currentWebview().tabCode;
 			_this.flowData();
+			_this.requestData();
+			switch(_this.tabCode) {
+                case 0:
+                    if(this.listParam.stepId == "300") {   // 100发起   200整改回复  300整改验证  400延期申请  500延期申请审批
+                        this.showVerify = true;
+                        this.showYanBtn = true;
+                        this.showButton =false;
+                        this.disabled = true;
+                        this.submitParam.rectificationSituation = this.listParam.rectificationSituation;
+                        this.submitParam.responsiblePerson = this.listParam.responsiblePerson;
+                        this.submitParam.completeDate = getNowFormatDate(this.listParam.completeDate.time); 
+                        this.submitParam.copyPerson = this.dangerData.copyPerson || "";
+                    } else if(this.listParam.stepId == "500") {
+                        this.showTongBtn = true;
+                        this.showButton =false;
+                    }
+                    break;
+                case 1:
+                    this.showButton = false;    // 隐藏底部按钮，只读
+                    this.disabled = true;
+                    break;
+                case 2:
+                    if(this.listParam.stepId == "500") {
+                        this.showBack = false;
+                    } else {
+                        this.showBack = true;
+                    }
+                    this.showButton = false;
+                    this.disabled = true;
+                    
+                    break;
+                case 3:
+                    this.showButton = false;
+                    break;
+                case 4:
+                    this.showButton = false;
+                    this.disabled = true;
+                    break;
+                case 5:
+                    this.showButton = false;
+                    this.disabled = true;
+                    break;
+            }
 		}
 		if (window.plus) {
 			plusReady()
@@ -19,6 +76,24 @@ new Vue({
 		}
     },
     methods: {
+		//详情
+		requestData: function() {
+			var param = {
+				"checkId": _this.listParam.id,
+			}
+			app.ajax({
+				url: app.INTERFACE.selectDanger,
+				data: param,
+				success: function(res) {
+					if (res.object.resultCode == "0") {
+						res.object.dangerList.reqcompletedate = sne.getNowFormatDate2(res.object.dangerList.reqcompletedate);
+						res.object.dangerList.distributdate = sne.getNowFormatDate2(res.object.dangerList.distributdate);
+						_this.dangerData = res.object.dangerList;
+					} else {
+					}
+				}
+			})
+		},
         delay: function() {
             sne.navigateTo({
             	url: "./5-5HSE.html",
@@ -28,7 +103,7 @@ new Vue({
 		flowData:function(){
 			plus.nativeUI.showWaiting();
 			var param = {
-				"recordNo": _this.recordNo
+				"recordNo": _this.listParam.recordNo,
 			}
 			app.ajax({
 				url: app.INTERFACE.hseCirculation,
